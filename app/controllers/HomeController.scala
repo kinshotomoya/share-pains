fe416036b42471fe76df19be856946251a9bb869
tomoya-kinsho <tomoya.kinsho@bizreach.co.jp>
2019-08-30 11:42:43 +0900

    post一覧画面完成

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,30 +1,32 @@
 package controllers
 
 import javax.inject._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.mvc._
+import slick.jdbc.JdbcProfile
+import tables.Tables._
+import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (val dbConfigProvider: DatabaseConfigProvider)(implicit val ec: ExecutionContext) extends Controller with HasDatabaseConfigProvider[JdbcProfile] with BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  def index = Action.async { implicit request =>
+    // .resultでDBIOAction型に変換している
+    db.run(Post.sortBy(_.createdAt.desc).result).map { posts =>
+      Ok(views.html.index(posts))
+    }
   }
 
   def generateTable = Action { implicit request =>
     slick.codegen.SourceCodeGenerator.main(
       Array("slick.jdbc.MySQLProfile", "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/sharepains?nullNamePatternMatchesAll=true", "./app", "tables", "user", "password")
     )
-    Ok(views.html.index())
+    Redirect(routes.HomeController.index())
   }
 }
